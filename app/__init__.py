5be2ff06dd1ab87097653104c14bb9700464056b
DurandSacha <sacha6623@gmail.com>
2020-11-12 18:56:17 +0100

    add Postres SQL config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from .views import app
-#from . import models
+#import models
+from models import db
 
 # Connect sqlalchemy to app
 #models.db.init_app(app)
@@ -9,3 +11,15 @@ from .views import app
 #@app.cli.command()
 #def init_db():
     #models.init_db()
+
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'bdd-drag-drop',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db.init_app(app)
---
+++ models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+# for install SQL postgres config
+# use https://blog.theodo.com/2017/03/developping-a-flask-web-app-with-a-postresql-database-making-all-the-possible-errors/ 
+
+
+class BaseModel(db.Model):
+    """Base data model for all objects"""
+    __abstract__ = True
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def __repr__(self):
+        """Define a base way to print models"""
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        """
+                Define a base way to jsonify models, dealing with datetime objects
+        """
+        return {
+            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
+
+class Station(BaseModel, db.Model):
+    """Model for the stations table"""
+    __tablename__ = 'table1'
+
+    id = db.Column(db.Integer, primary_key = True)
+    n1 = db.Column(db.Float)
+    n2 = db.Column(db.Float)
