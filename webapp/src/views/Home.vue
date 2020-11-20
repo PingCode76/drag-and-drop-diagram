ccbfe57ad86c75a87db7692274630d9444e5f0bd
Yohann76 <yohanndurand76@gmail.com>
2020-11-20 16:40:41 +0100

    [refactoring] create Function.vue

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -3,83 +3,45 @@
     <BaseComponent title="Application Template"/>
     <p> {{ title }} </p>
     <p> {{ APImessageGreeting }}</p>
+
     <div class="drag-left">
+
       <button type="button" class="btn btn-danger">
         Export Data
       </button>
+
       <h4> Element </h4>
       <hr class="hr-80">
-      <h7> add label </h7>
-
+    
       <!-- label transition -->
+      <h7> add label </h7>
       <transition-group name="list" tag="div">
         <drag v-for="n in numbers" :key="n" class="label" :data="n" @cut="remove(n)">{{n}}</drag>
       </transition-group>
+      <hr class="hr-80">
       <!-- end label transition -->
 
-      <hr class="hr-80">
-      <h7> add nodes </h7>
-      <hr class="hr-80">
-      <h7> add function </h7>
-
       <!-- label transition -->
+      <h7> add function </h7>
       <transition-group name="list" tag="div">
         <drag v-for="f in functions" :key="f" class="functions" :data="f" @cut="remove(f)">{{f}}</drag>
       </transition-group>
+      <hr class="hr-80">
       <!-- end label transition -->
 
+      <!-- node transition -->
+      <h7> add nodes </h7>
       <hr class="hr-80">
+      <!-- end node transition -->
+      
     </div>
     <div class="container">
       <div class="container-diagram">
         <h4> {{ title }} </h4>
 
-      
-        <!-- function drag label in function -->
-        <drop class="copyFunction" @drop="onCopyDropFunctions">
+        <!-- component function --> 
+        <function></function>
 
-          <span v-for="(f, function1) in copiedFunction" :key="function1">
-          
-            <transition-group name="list" tag="div">
-              <!-- drag insert -->
-              <drag v-for="f in functions" :key="f" class="function" :data="f" @cut="remove(f)">{{f}}
-              
-                <div class="row">
-                  <div class="function">
-                    <div class="group">
-                      <drop class="copyLabel" @drop="onCopyDropLabel">
-                        <span v-for="(n, index) in copied" :key="index">
-                          <transition-group name="list" tag="div">
-                            <drag v-for="n in numbers" :key="n" class="label" :data="n" @cut="remove(n)">{{n}}</drag>
-                          </transition-group>
-                        </span>
-                      </drop>
-                      <drop class="copyLabel" @drop="onCopyDropLabel1">
-                        <span v-for="(n, index1) in copied1" :key="index1">
-                          <transition-group name="list" tag="div">
-                            <drag v-for="n in numbers" :key="n" class="label" :data="n" @cut="remove(n)">{{n}}</drag>
-                          </transition-group>
-                        </span>
-                      </drop>
-                      <drop class="copyLabel" @drop="onCopyDropLabel2">
-                        <span v-for="(n, index2) in copied2" :key="index2">
-                          <transition-group name="list" tag="div">
-                            <drag v-for="n in numbers" :key="n" class="label" :data="n" @cut="remove(n)">{{n}}</drag>
-                          </transition-group>
-                        </span>
-                      </drop>
-                    </div>
-                  </div> <!-- end function-->
-                </div> <!-- end row -->
-
-              </drag>  
-              <!-- drag insert end -->
-
-            </transition-group>
-          </span>
-        </drop>
-        <!-- end function drag label in function -->
-        
       </div> <!-- end container diagram-->
     </div> <!-- end container -->
   </div> <!-- end home-->
@@ -89,54 +51,21 @@
 // @ is an alias to /src
 import { Drag, Drop, DropMask } from "vue-easy-dnd";
 import BaseComponent from '../components/BaseComponent.vue'
+import Function from '../components/Function.vue'
 
   export default {
     name: 'Home',
     components: {
       BaseComponent,
       Drag,
-      Drop,
+      Function,
     },
     data: function(){
         return {
             APImessageGreeting: '',
-            numbers: ["label"], // label numer 
-            numbers1: ["label"],
-            numbers2: ["label"],
-            copied: [], // label copied number 
-            copied1: [],
-            copied2: [],
-            cut: [],
-            functions: ["function"], // function
-            copiedFunction: [], // function 
+            numbers: ["label"], // label number ( unique drag )
+            functions: ["function"], // function ( unique drag ) 
         }
-    },
-    methods: {
-      onCopyDropLabel(e) {
-        this.copied.push(e.data);
-      },
-      onCopyDropLabel1(e) {
-        this.copied1.push(e.data);
-      },
-      onCopyDropLabel2(e) {
-        this.copied2.push(e.data);
-      },
-      onCopyDropFunctions(e) {
-        this.copiedFunction.push(e.data);
-      },
-      remove(n) {
-        let index = this.numbers.indexOf(n);
-        this.numbers.splice(index, 1);
-
-        let index1 = this.numbers1.indexOf(n);
-        this.numbers1.splice(index1, 1);
-
-        let index2 = this.numbers2.indexOf(n);
-        this.numbers2.splice(index2, 1);
-
-        let function1 = this.functions.indexOf(f);
-        this.functions.splice(function1, 1)
-      }
     },
     created: async function(){
         const gResponse = await fetch("http://localhost:5000/greeting");
@@ -147,6 +76,7 @@ import BaseComponent from '../components/BaseComponent.vue'
 </script>
 
 <style>
+
 /* item drag and drop */ 
 .label {
   width: 170px;
@@ -161,6 +91,21 @@ import BaseComponent from '../components/BaseComponent.vue'
   margin-bottom : 10px ; 
 }
 
+/* item drag and drop */ 
+.functions {
+  width: 170px;
+  height: 60px;
+  background-color: blue;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px 10px 0 10px;
+  font-size: 20px;
+  transition: all 0.5s;
+  margin-bottom : 10px ; 
+}
+
+/* other */
 .copyLabel::before {
   content: "add label"; 
   position: absolute;
@@ -186,54 +131,7 @@ import BaseComponent from '../components/BaseComponent.vue'
   flex: 1;
   background-color: green;
 }
-/* function element copied in nav */
-.functions {
-  width: 170px;
-  height: 60px;
-  background-color: blue;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px 10px 0 10px;
-  font-size: 20px;
-  transition: all 0.5s;
-  margin-bottom : 10px ; 
-}
 
-.copyFunction {
-  width: 100%;
-  margin: 20px 10px;
-  border: 1px solid black;
-  min-height: 100px;
-  height: auto;
-  display: inline-block;
-  position: relative;
-  flex: 1;
-  border : 3px grey solid ;
-  /* background-color: rgba(25,25,25,0.2); */
-}
-
-.copyFunction::before {
-  content: "add function"; 
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  color: rgba(0, 0, 0, 0.4);
-  font-size: 25px;
-  font-weight: bold;
-}
-
-/* div function */ 
-.function {
-    width: 95%;
-    min-height: 150px;
-    height: auto;
-    margin : auto;
-    margin-top: 10px;
-    margin-bottom: 45px ; 
-    background-color : blue; 
-}
 
 .cut {
   margin: 20px 10px;
