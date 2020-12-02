cca506a0136f5c662bc7c2ef89e18ea7b29e0893
DurandSacha <sacha6623@gmail.com>
2020-12-02 21:11:41 +0100

    update API with CRUD label

--- api/apiFunction.py
+++ api/apiFunction.py
@@ -1,9 +1,10 @@
 import math
 from models import db, Label
 
-def addLabelAPI(nb1,nb2):
+#return addLabelAPI(escape(title),escape(node1),escape(node2),escape(functionNumber), escape(column))
+def addLabelAPI(title,node1,node2,functionNumber,column):
     #db.create_all()
-    Label1 = Label(var1=nb1,var2=nb2)
+    Label1 = Label(title=title, node1=node1, node2=node2, functionNumber=functionNumber, column=column)
     db.session.add(Label1)
     db.session.commit()
-    return {"labelStatus": "added in DB"}
+    return {"labelStatus": "added in database"}
--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from apiFunction import addLabelAPI
 from markupsafe import escape
 import models
+from models import db, Label
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -18,8 +19,36 @@ def home():
 def greeting():
     return {"greeting": "Hello from Flask API"}
 
-@app.route("/addLabel/<nb1>/<nb2>")
-def addLabel(nb1,nb2):
-    return addLabelAPI(escape(nb1),escape(nb2))
-    
+# Route : /addLabel/{title}/{node1}/{node2}/{functionNumber}/{column}
+@app.route("/addLabel/<string:title>/<string:node1>/<string:node2>/<int:functionNumber>/<int:column>", methods=['GET'])
+def addLabel(title,node1,node2,functionNumber,column):
+    # try this route: addLabel/loremIpsum/BR45/CV10/1/1
+    return addLabelAPI(str(title), str(node1), str(node2), int(functionNumber), int(column))
 
+# Route : /removeLabel/{id}
+@app.route("/removeLabel/<int:id>")
+def removeLabel(id):
+    label = Label.query.get(id)
+    db.session.delete(label)
+    db.session.commit()
+    return {"database": "label" + str(id) + "has been deleted"}
+
+# Route : /label/{id}
+@app.route("/label/<int:id>")
+def labelInformation(id):
+    label = Label.query.get(id)
+    if(label):
+        return {"id": str(id), "title": label.title}
+    else:
+        return {"database" : "Label"+ str(id) + " not exist"}
+    
+# Route : /labels
+@app.route("/labels")
+def listLabels():
+    dict = {}
+    labels = Label.query.all()
+    for label in labels:
+        dict.update({label.id : label.title})
+    return dict
+
+#TODO: update label ?
